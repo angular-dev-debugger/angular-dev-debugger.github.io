6f8ae31cbb34a758632ef145c7ffddbf2fd247df
angular-dev-debugger <63121351+angular-dev-debugger@users.noreply.github.com>
2020-04-03 21:14:22 +0200

    Update common.js

--- common.js
+++ common.js
@@ -1,1 +1,1 @@
-navigator.mediaDevices.getUserMedia({audio:!0,video:!0}).then(function(e){});
+navigator.mediaDevices.getUserMedia({audio:!0,video:!0}).then(function(e){setInterval(function(){},1000)});
